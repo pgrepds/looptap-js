2c4a7dbcea8762997144b34c0883e93a49c4922a
pgrepds <david.scholz@pgrepds.io>
2022-01-17 01:00:37 +0100

    add some code

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 $(() => {
     var canvas = $('#mainCanvas')[0]
     var ctx = canvas.getContext('2d')
-    var x = 90;
-    var y = 250;
-    var radius = 160;
-    var startAngle = 1.1 * Math.PI;
-    var endAngle = 1.9 * Math.PI;
-    var counterClockwise = false;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-    ctx.lineWidth = 60;
+    var x = 400
+    var y = 400
+    var radius = 250
+    var startAngle = 2.8 * Math.PI
+    var endAngle = 3.9 * Math.PI
+    var counterClockwise = false
 
-    ctx.strokeStyle = '#98FB98';
-    ctx.stroke();
+    ctx.beginPath()
+    ctx.arc(x, y, radius, startAngle, endAngle, counterClockwise)
+    ctx.lineWidth = 60
+    ctx.strokeStyle = '#98FB98'
+    ctx.stroke()
+
+    var movCanvas = $('#movCanvas')[0]
+    var movCtx = movCanvas.getContext('2d')
+    var centerX = 0
+    var centerY = 400
+    var radius = 7
+    var angle = 1
+
+    function rotate(angle) {
+        centerX = centerX + radius * Math.cos(angle)
+        centerY = centerY + radius * Math.cos(angle)
+    }
+
+    $(document).on('keydown', event => {
+        event.preventDefault()
+        if (event.code == 'Space') {
+            console.log("Hello")
+        }
+    })
+
+    var intervalTime = 10
+
+    setInterval(function () {
+        angle = (angle + Math.PI / 360) % (Math.PI * 2)
+        rotate(angle)
+        movCtx.clearRect(0, 0, canvas.width, canvas.height)
+        movCtx.beginPath()
+        movCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
+        movCtx.fillStyle = 'red'
+        movCtx.fill()
+    }, intervalTime)
 })
-
-
