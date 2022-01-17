8df88a516cc57d75991edcc9efe1b5b71d2379de
pgrepds <david.scholz@pgrepds.io>
2022-01-17 02:11:11 +0100

    check if point is on screen

--- app.js
+++ app.js
@@ -8,18 +8,13 @@ $(() => {
     var endAngle = 3.9 * Math.PI
     var counterClockwise = false
 
-    ctx.beginPath()
-    ctx.arc(x, y, radiusArc, startAngle, endAngle, counterClockwise)
-    ctx.lineWidth = 60
-    ctx.strokeStyle = '#98FB98'
-    ctx.stroke()
-
     var movCanvas = $('#movCanvas')[0]
     var movCtx = movCanvas.getContext('2d')
     var centerX = 0
     var centerY = 400
-    var radius = 7
-    var angle = 0.05
+    var radius = 30
+    var angle = 0
+    var speed = 0.01
 
     function rotate(angle) {
         centerX = x + radiusArc * Math.cos(angle)
@@ -29,19 +24,27 @@ $(() => {
     $(document).on('keydown', event => {
         event.preventDefault()
         if (event.code == 'Space') {
-            console.log("Hello")
+            if (ctx.isPointInPath(centerX, centerY)) {
+                console.log("match1")
+            }
         }
     })
 
-    var intervalTime = 1
-
+    var intervalTime = 10
     setInterval(function () {
-        angle = (angle + Math.PI / 360) % (Math.PI * 2)
+
+        ctx.beginPath()
+        ctx.arc(x, y, radiusArc, startAngle, endAngle, counterClockwise)
+        ctx.lineWidth = 60
+        ctx.strokeStyle = '#F08080'
+        ctx.stroke()
+
+        angle += speed
         rotate(angle)
         movCtx.clearRect(0, 0, canvas.width, canvas.height)
         movCtx.beginPath()
         movCtx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
-        movCtx.fillStyle = 'red'
+        movCtx.fillStyle = '#2F4F4F'
         movCtx.fill()
     }, intervalTime)
 })
